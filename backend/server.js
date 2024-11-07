604efc446403f79accd1916601f4cad9664e6ddb
Phillip <90676461+phillip-niakoulov@users.noreply.github.com>
2024-11-07 08:36:39 +0000

    Revert port

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const userRoutes = require('./routes/userRoutes');
 const cartRoutes = require('./routes/cartRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
