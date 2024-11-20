d711ca05b0d11ca36677c47c3ec897edb4b06ea1
Phillip <90676461+phillip-niakoulov@users.noreply.github.com>
2024-11-20 01:21:11 +0000

    Log env variables for testing

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+console.log(`PORT: ${process.env.PORT}, BACKEND_URL: ${process.env.BACKEND_URL}, DATABASE_URL: ${process.env.DATABASE_URL}, VITE_API_BACKEND_PORT: ${process.env.VITE_API_BACKEND_PORT}, VITE_API_BACKEND_URL: ${process.env.VITE_API_BACKEND_URL}`)
+
 mongoose
     .connect(process.env.DATABASE_URL)
     .then(() => console.log('MongoDB connected'))
